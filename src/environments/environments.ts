06226e3d501af2149ada3d876af39e26e40e2136
ryanmiuracarrasco <ryanmiuracarrasco@gmail.com>
2024-12-12 20:26:24 +0000

    initial commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyA-mAXWr0k4RLAgyKP3YBRB1JPrCDvGbt0',
+      authDomain: 'testegemini-5f266.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'testegemini-5f266',
+      storageBucket: 'testegemini-5f266.firebasestorage.app',
+      messagingSenderId: '702324080275',
+      appId: '1:702324080275:web:0f46044ee11c5d84061766',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyA3lY_3ipgbXGVw_zNVC7PIhKUsOobQAG4',
+  debug_token: ''
+};
